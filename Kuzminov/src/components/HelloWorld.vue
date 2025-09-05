d3f56a2be02d20f73fd58d8b8a541b5dc1deffa9
Sizllle <vladislaw.kuzminov2017@gmail.com>
2025-09-05 20:01:21 +0300

    Vue.js lab 1

---
+++ Kuzminov/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="center">
+    <h1>Інженерія програмного забезпечення</h1>
+    <h1>4 курс</h1>
+    <h1>Група 8.1214</h1>
+    <h1>Кузьмінов Владислав Олександрович</h1>
+  </div>
+</template>
+
+<style scoped>
+.center {
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  font-size: 16px;
+  color: #1e1e2c;
+}
+</style>
